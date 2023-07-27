e7eec5b3f17717b9b9fec6fd7bddf0d623d38c71
Vikramaditya G Khupse <vikramadityakhupse@gmail.com>
2023-07-27 18:19:05 +0530

    added main logic for getting pic from website.

--- InstaBot/pexels_image.py
+++ InstaBot/pexels_image.py
@@ -1,30 +1,69 @@
 import requests
 import random
 import tkinter as tk
+from PIL import Image, ImageTk
+from io import BytesIO
 
-
-#Imposrtant links
+# Important links
 url = "https://api.pexels.com/v1/curated"
 headers = {"Authorization": "wl80YOohp4aeCMdLTwlt2iFushjwq4biU42LT4tXPBVHoWLyO4Og0eBk"}
 
-#Getting Response
-response = requests.get(url, headers= headers).json()
+# Getting Response
+response = requests.get(url, headers=headers).json()
 
-#Generating Random number to pick a random image
-already_picked_num = []
-num = None
-for number in num:
-    if number in already_picked_num:
-        num = random.randrange(1, 15)
-        already_picked_num.append(num)
+# Generating Random number to pick a random image
+num = random.randrange(1, 15)
 
 
+def give_image(image_number):
+    image_id = response['photos'][image_number]["id"]
+    photo_url = response['photos'][image_number]["src"]["original"]
+    photographer = response['photos'][image_number]["photographer"]
+
+    return image_id, photo_url, photographer
 
 
+photo_id, link, name, = give_image(num)
+print("Photo id : {}\nPhoto link: {}\nClicked by : {}".format(photo_id, link, name))
 
-photo_id = response['photos'][num]["id"]
-photo_url = response['photos'][num]["src"]["large"]
-photographer = response['photos'][num]["photographer"]
 
-print(f"Photo id : {photo_id}\nPhoto link: {photo_url}\nClicked by : {photographer}")
+def get_image(photo_url):
+    image_response = requests.get(photo_url)
+    raw_image = Image.open(BytesIO(image_response.content))
+    return raw_image
 
+
+def view_image():
+    root = tk.Tk()
+    root.title("Todays Quote")
+    window_width = 1920
+    window_height = 1080
+
+
+    root.geometry(f"{window_width}x{window_height}")
+    final_image = get_image(link)
+
+    if final_image:
+        # Resize the image to fit the window
+        image = final_image.resize((window_width, window_height), Image.LANCZOS)
+
+        # Convert the image to a PhotoImage object
+        photo = ImageTk.PhotoImage(image)
+
+        # Create a Label widget to display the image
+        image_label = tk.Label(root, image=photo)
+
+        # Keep a reference to the PhotoImage to prevent it from being garbage collected
+        image_label.image = photo
+
+        # Pack the image label to make it visible in the window
+        image_label.pack(pady=20)
+    else:
+        # If no image is available, display an error message
+        error_label = tk.Label(text="Error fetching the image from Pexels API.")
+        error_label.pack(pady=20)
+
+    root.mainloop()
+
+
+view_image()
